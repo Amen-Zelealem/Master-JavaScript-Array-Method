fdf38d79ea3f89459f5392af483b957a9caae809
Amen Zelealem <amenzelealemtadese@gmail.com>
2024-12-04 10:32:45 +0300

    First Commit about TO String Method

---
+++ README.md
@@ -0,0 +1,7 @@
+I’m sharing a comprehensive JavaScript array methods cheat sheet to help developers better understand and utilize these powerful tools. 
+It includes all key methods like 
+    map(), 
+    filter(), 
+    reduce(), 
+    forEach(), and many more, along with practical use cases.
+# Stay Tuned!
---
+++ index.js
@@ -0,0 +1,17 @@
+// To String Method
+
+// The to String Method toString(), returns a string with array values separated by Commas.
+// And It Does NOT change the original Array
+
+// Syntax 👇🏼👇🏼👇🏼
+
+// array.toString()
+
+// Example :
+
+let bikes = ["Yamaha", "Bajaj", "Honda", "TVS"];
+
+console.log(bikes.toString()); 
+
+//Output
+// Yamaha, Bajaj, Honda, TVS
